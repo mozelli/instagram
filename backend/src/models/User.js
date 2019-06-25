ce94e49fbf9519255ae92ed4ff6997a2e4554d83
mozelli <joaomozelli@hotmail.com>
2019-06-24 21:23:00 -0300

    Authentication system

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,13 +1,35 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  birthday: Date,
+  name: {
+  	type: String,
+  	required: true,
+  },
+  email: {
+  	type: String,
+  	required: true,
+  	lowercase: true,
+  },
+  password: {
+  	type:	String,
+  	required: true,
+  	select: false,
+  },
+  birthday: {
+  	type: Date,
+  	required: true,
+  },
 },
 {
   timestamps: true,
+});
+
+UserSchema.pre('save', async function(next) {
+	const hash = await bcrypt.hash(this.password, 10);
+	this.password = hash;
+
+	next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const PostController = require('./controllers/PostController');
 const LikeController = require('./controllers/LikeController');
 const LoginController = require('./controllers/LoginController');
 const UserController = require('./controllers/UserController');
+const AuthController = require('./controllers/AuthController');
 
 const routes = new express.Router();
 const upload = multer(uploadsConfig);
@@ -16,6 +17,7 @@ routes.delete('/posts/:id', PostController.delete);
 
 //routes.get('/login', LoginController.login);
 routes.post('/users', UserController.store);
-
+routes.post('/register', AuthController.index);
+routes.post('/authenticate', AuthController.authenticate);
 
 module.exports = routes;
